4094af7b97b4df8402bfd71aa5e3eb2b89d3e57b
LuongDV <dangvluong@gmail.com>
2021-09-20 23:46:44 +0700

    Rewrite NavigationTagHelper

--- WebApp/Helper/PaginationTagHelper.cs
+++ WebApp/Helper/PaginationTagHelper.cs
@@ -18,56 +18,88 @@ namespace WebApp.Helper
             output.Attributes.Add("class", "pagination mx-auto");
             StringBuilder sb = new StringBuilder();
 
-            if (CurrentPage == null)
+            if (TotalPage <= 5)
             {
-                string uri = string.Format(Url, "");
-                sb.AppendFormat("<li class=\"page-item active\"><a href=\"{1}\" class=\"page-link\">{0}</a></li>", 1, uri);
-                for (int i = 2; i <= 5; i++)
+                if (CurrentPage is null)
                 {
-                    uri = string.Format(Url, i);
-                    sb.AppendFormat("<li class=\"page-item\"><a href=\"{1}\" class=\"page-link\">{0}</a></li>", i, uri);
+                    AddPageItem(sb, isActive: true);
+                    AddMultiPageItem(sb, 1, 2, TotalPage);                    
+                    if (TotalPage > 1)                    
+                        AddNavigation(sb, 1);                    
                 }
-
-                sb.AppendFormat("<button class=\"page-link\">...</button>");
-                uri = String.Format(Url, TotalPage);
-                sb.AppendFormat("<li class=\"page-item\"><a href=\"{1}\" class=\"page-link\">{0}</a></li>", TotalPage, uri);
+                else
+                {
+                    int currentPage = Convert.ToInt32(CurrentPage);
+                    AddNavigation(sb, currentPage, true);
+                    AddPageItem(sb);
+                    AddMultiPageItem(sb, currentPage, 2, 5);
+                    if (currentPage < TotalPage)                    
+                        AddNavigation(sb, currentPage);
+                    
+                }
             }
             else
             {
-                int currentPage = Convert.ToInt32(CurrentPage);
-                string uri = string.Format(Url, "");
-                sb.AppendFormat("<li class=\"page-item\"><a href=\"{1}\" class=\"page-link\">{0}</a></li>", 1, uri);
-                if (currentPage >= 5)
+                if (CurrentPage is null)
                 {
-                    sb.AppendFormat("<button class=\"page-link\">...</button>");
+                    AddPageItem(sb, isActive: true);
+                    AddMultiPageItem(sb, 1, 2, 5);
+                    AddNavigation(sb, 1);
                 }
-                for (int i = currentPage - 2; i <= currentPage + 4 && i <= TotalPage; i++)
+                else
                 {
-                    if (i == 0 || i == 1 || i == currentPage - 1 && currentPage == 2)
-                    {
-                        continue;
-                    }
-                    uri = string.Format(Url, i);
-                    if (i == currentPage)
-                    {
-                        sb.AppendFormat("<li class=\"page-item active\"><a href=\"{1}\" class=\"page-link\">{0}</a></li>", i, uri);
-                    }
-                    else
-                    {
-                        sb.AppendFormat("<li class=\"page-item\"><a href=\"{1}\" class=\"page-link\">{0}</a></li>", i, uri);
-                    }
+                    int currentPage = Convert.ToInt32(CurrentPage);
+                    int j = currentPage;
+                    if (currentPage - 1 == 0)                    
+                        j += 2;                    
+                    if (currentPage - 2 == 0)                    
+                        j++;                    
+                    if (TotalPage - currentPage == 1)                    
+                        j -= 1;                    
+                    if (TotalPage - currentPage == 0)                    
+                        j -= 2;                    
+                    if (currentPage > 1)                    
+                        AddNavigation(sb, currentPage, true);                    
+                    AddMultiPageItem(sb, currentPage, j - 2, j + 2);                    
+                    if (currentPage < TotalPage)                    
+                        AddNavigation(sb, currentPage);                    
                 }
-                if (currentPage <= TotalPage - 6)
-                {
-                    sb.AppendFormat("<button class=\"page-link\">...</button>");
-                }
-                if (currentPage + 4 < TotalPage)
-                {
-                    uri = string.Format(Url, TotalPage);
-                    sb.AppendFormat("<li class=\"page-item\"><a href=\"{1}\" class=\"page-link\">{0}</a></li>", TotalPage, uri);
-                }                
-            }            
+            }
             output.Content.SetHtmlContent(sb.ToString());
+        }
+
+        private void AddMultiPageItem(StringBuilder sb, int currentPage, int min, int max)
+        {
+            for (int i = min; i <= max; i++)
+            {
+                if (i == currentPage)                
+                    AddPageItem(sb, i, true);                
+                else                
+                    AddPageItem(sb, i);                
+            }
+        }
+
+        private void AddPageItem(StringBuilder sb, int? i = null, bool isActive = false)
+        {
+            var uri = string.Format(Url, i);
+            var active = "";
+            if (i is null)
+                i = 1;
+            if (isActive)
+                active = "active";
+            sb.AppendFormat("<li class=\"page-item {2}\"><a href=\"{1}\" class=\"page-link\">{0}</a></li>", i, uri, active);
+        }
+        private void AddNavigation(StringBuilder sb, int currentPage, bool backward = false)
+        {
+            int i = currentPage + 1;
+            string iconClass = "fas fa-fast-forward";
+            if (backward)
+            {
+                i = currentPage - 1;
+                iconClass = "fas fa-fast-backward";
+            }
+            var uri = string.Format(Url, i);
+            sb.AppendFormat($"<li class=\"page-item\"><a href=\"{uri}\" class=\"page-link\"><i class=\"{iconClass}\"></i></a></li>");
         }
     }
 }
