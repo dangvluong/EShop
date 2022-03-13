4f91785ef910335e73403014a6aee429ca063f01
LuongDV <dangvluong@gmail.com>
2022-03-13 11:09:51 +0700

    Add encrypt and decrypt methods

--- WebApp/Helper/SiteHelper.cs
+++ WebApp/Helper/SiteHelper.cs
@@ -1,12 +1,60 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace WebApp.Helper
 {
-    public static class SiteHelper
+    public class SiteHelper
     {
+        private const string key = "sjfpqisjf9813j23";
+        public static string EncryptString(string plaintext)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+            using(Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plaintext);
+                        }
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(array);
+        }
+
+        public static string DecryptString(string cipherText)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
         public static byte[] HashPassword(string plaintext)
         {
             HashAlgorithm algorithm = HashAlgorithm.Create("SHA-512");
